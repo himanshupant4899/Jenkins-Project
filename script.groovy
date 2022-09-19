00ee3af39da430b4d3de43236f7089f45c2f8880
Himanshu Pant <himanshupant4899@gmail.com>
2022-09-19 10:55:00 +0530

    syntax error

--- script.groovy
+++ script.groovy
@@ -2,12 +2,14 @@ def buildJar(){
     echo "Building the application"
     sh 'mvn package'
 }
+
 def buildImage(){
     echo "Building the docker image"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo',passwordVariable: 'PASS',usernameVariable: 'USER')]){
     sh 'docker build -t himanshupant4899/demo-app:jma-1.0 .'
     sh "echo $PASS | docker login -u $USER --password-stdin"
     sh 'docker push himanshupant4899/demo-app:jma-1.0'
+}
 }
 def deployApp(){
     echo "Deploying the application"
